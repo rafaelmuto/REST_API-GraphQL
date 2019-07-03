db07695f4f978d148b925e6dc5156353d44eae1b
Rafael Muto <r.nagahama@gmail.com>
2019-07-03 00:23:23 -0300

    OPTIONS method intercepter,

--- app.js
+++ app.js
@@ -51,6 +51,9 @@ app.use((req, res, nxt) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
   nxt();
 });
 
